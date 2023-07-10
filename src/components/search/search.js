73e90b45f537fa3258bfc4f3b930e3f394889f18
Ihor Lev <igorlevin000@gmail.com>
2023-07-11 00:15:19 +0300

    create Header

---
+++ src/components/search/search.js
@@ -0,0 +1,11 @@
+import { DivComponent } from "../../common/div-component";
+import './search.css';
+
+export class Search extends DivComponent {
+    constructor(state) {
+        super();
+        this.appState = state;
+    }
+
+
+}
